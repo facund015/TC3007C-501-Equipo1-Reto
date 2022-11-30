2ea371389c3426d677cfc0df1b233f4f8c44f898
facund015 <facundo.vecchi@outlook.com>
2022-11-30 00:32:50 -0600

    Minor changes

--- AplicacionWeb/inegi_bot/src/components/ChatBot/ChatBot.jsx
+++ AplicacionWeb/inegi_bot/src/components/ChatBot/ChatBot.jsx
@@ -8,7 +8,7 @@ export const ChatBot = ({setActiveGraph}) => {
         text: "Hola, soy INEGI BOT, ¿en qué te puedo ayudar?", type: "simple", user: false
     }]);
     const inputRef = createRef();
-    const apiURL = "http://127.0.0.1:5000/master_funct/";
+    const apiURL = "http://127.0.0.1:5000/query/";
 
     const inputEnterPress = (event) => {
         if (event.key === "Enter" && !chatLog[chatLog.length - 1].user) {
