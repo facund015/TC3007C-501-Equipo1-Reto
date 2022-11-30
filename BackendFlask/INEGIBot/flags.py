0a443b44a65c9dd339711dd7700b868a158911a4
facund015 <facundo.vecchi@outlook.com>
2022-11-30 00:30:41 -0600

    Integracion Completa de Neuraan y frontend

--- BackendFlask/INEGIBot/flags.py
+++ BackendFlask/INEGIBot/flags.py
@@ -99,18 +99,18 @@ def get_pob_message(clave, tabla, nombregeo, spark, campos_pob): #ajustar cambio
     pob_m = pob_m.toPandas().to_numpy()
     if tabla == 'estados':
         nom_message = str(pob_m[0][0])
-        pob_message = str(int(pob_m[0][1]))
+        pob_message = add_commas(str(int(pob_m[0][1])))
         return nom_message, pob_message
     else:
         nom_message = str(pob_m[0][0]) + ', ' + str(pob_m[0][1])
-        pob_message = str(int(pob_m[0][2]))
+        pob_message = add_commas(str(int(pob_m[0][2])))
         return nom_message, pob_message
 
 
 def get_compatibles(filtro, desglose):
-    if (filtro == 'hombre' or filtro == 'mujer') and desglose == 'edad':
+    if (filtro == 'Masculina' or filtro == 'Femenina') and desglose == 'Edad':
         compatible = True
-    elif (filtro == 'infantil' or filtro == 'joven' or filtro == 'adulto' or filtro == 'mayor') and desglose == 'sexo':
+    elif (filtro == 'Infantil' or filtro == 'Juvenil' or filtro == 'Adulta' or filtro == 'Tercera edad') and desglose == 'Sexo':
         compatible = True
     else:
         compatible = False
@@ -123,21 +123,27 @@ campos_pob_general = 'POB3, POB26, POB27, POB28, POB29, POB30, POB31, POB32, POB
 campos_sexo = 'POB42, POB84'
 label_edad = ['0:4','5:9','10:14','15:19','20:24','25:29','30:34','35:39','40:44','45:49','50:54','55:59','60:64','65:69','70:74','75:79','80:84','85+']
 label_sexo = ['Hombres', 'Mujeres']
-campos_filtro_dic = {'hombre':'POB84', 'mujer':'POB42', 'infantil':'POB8', 'joven':'POB11', 'adulto': '(POB14 + POB15) AS adulto', 'mayor':'POB23'}
-str_filtro_dic = {'hombre':'de hombres', 'mujer':'de mujeres', 'infantil':'infantil(0-14)', 'joven':'de jovenes(15-29)', 'adulto': 'adulta(30-59)', 'mayor':'mayor(60+)'}
+campos_filtro_dic = {'Masculina':'POB84', 'Femenina':'POB42', 'Infantil':'POB8', 'Juvenil':'POB11', 'Adulta': '(POB14 + POB15) AS adulto', 'Tercera edad':'POB23'}
+str_filtro_dic = {'Masculina':'masculina', 'Femenina':'femenina', 'Infantil':'infantil(0-14)', 'Juvenil':'juvenil(15-29)', 'Adulta': 'adulta(30-59)', 'Tercera edad':'tercera edad(60+)'}
 #json_flag = {'municipio': 'Monterrey',  #Diccionario de pruebas para ingresar parametros
 #    'estado': 'Nuevo Leon',
 #    'filtro': 'adulto',  # lista disponible(hombre, mujer, infantil, joven, adulto, mayor)
 #    'desglose': 'sexo'}  # lista diponible(edad, sexo)
 
+def add_commas(n):
+    n = str(n)
+    if len(n) <= 3:
+        return n
+    else:
+        return add_commas(n[:-3]) + ',' + n[-3:]
 
 def flag_alert(json_flag, spark):
     json_flag['estado'], json_flag['municipio'] = clean_str(json_flag['estado'], json_flag['municipio'])
     df_mun = pd.read_csv('BackendFlask\INEGIBot\input\df_diccionario_mun.csv')
-    filtro_list = ['hombre', 'mujer', 'infantil', 'joven', 'adulto', 'mayor']
+    filtro_list = ['Masculina', 'Femenina', 'Infantil', 'Juvenil', 'Adulta', 'Tercera edad']
     municipio_flag = (df_mun.NOMGEO2 == json_flag['municipio']).any()
     filtro_flag = json_flag['filtro'] in filtro_list
-    desglose_flag = json_flag['desglose'] in ['edad', 'sexo']
+    desglose_flag = json_flag['desglose'] in ['Edad', 'Sexo']
     clave, tabla, nombregeo = define_geo(json_flag, municipio_flag, spark)  # Se obtienen la clave del municipio/estado y su nombre
     compatible = get_compatibles(json_flag['filtro'], json_flag['desglose'])  # variable para determinar si es compatible el filtro con el desglose en caso negativo se muestra solo el filtro
     if filtro_flag == False and desglose_flag == False: #doble desglose general
@@ -147,49 +153,49 @@ def flag_alert(json_flag, spark):
         messages = to_json_doble_desglose(nom_message, pob_message, label_edad, data_pob_hom, data_pob_muj)
         return messages
     elif filtro_flag == False and desglose_flag == True: #desglose simple
-        if json_flag['desglose'] == 'edad':  # aplicando desglose por edad
+        if json_flag['desglose'] == 'Edad':  # aplicando desglose por edad
             data_pob_gen = pob_edad(clave, tabla, campos_pob_general, spark)
             nom_message, pob_message = get_pob_message(clave, tabla, nombregeo, spark, 'POB1')
-            message_str = "En 2020 la poblacion de %s era de %s personas. A continuacion se muestra la poblacion desglosada por edad" % (nom_message, pob_message)
+            message_str = "En 2020 la poblacion de %s era de %s personas. A continuacion se muestra la poblacion desglosada por edad." % (nom_message, pob_message)
             messages = to_json_simple_desglose(message_str, label_edad, data_pob_gen)
             return messages
         else:  # aplicando desglose por sexo
             data_pob_sex = pob_edad(clave, tabla, campos_sexo, spark) 
             nom_message, pob_hombres_message = get_pob_message(clave, tabla, nombregeo, spark, 'POB84')
             nom_message, pob_mujeres_message = get_pob_message(clave, tabla, nombregeo, spark, 'POB42')
-            message_str = "En 2020 la poblacion de hombres en %s era de %s y la de mujeres era de %s" % (nom_message,pob_hombres_message, pob_mujeres_message)
+            message_str = "En 2020 la poblacion masculina en %s era de %s y la femenina era de %s." % (nom_message,pob_hombres_message, pob_mujeres_message)
             messages = to_json_simple_desglose(message_str, label_sexo, data_pob_sex)
             return messages
     elif filtro_flag == True and desglose_flag == True and compatible == True: # aplicando filtro y desglose
         campos_filtro = campos_filtro_dic[json_flag['filtro']] # campos necesarios para cada filtro
-        if json_flag['desglose'] == 'edad':  # aplicando desglose por edad
-            if json_flag['filtro'] == 'hombre':
+        if json_flag['desglose'] == 'Edad':  # aplicando desglose por edad
+            if json_flag['filtro'] == 'Masculina':
                 data_pob = pob_edad(clave, tabla, campos_pob_hombres, spark)
-            if json_flag['filtro'] == 'mujer':
+            if json_flag['filtro'] == 'Femenina':
                 data_pob = pob_edad(clave, tabla, campos_pob_mujeres, spark)
             nom_message, pob_filtro_message = get_pob_message(clave, tabla, nombregeo, spark, campos_filtro)
-            message_str = "En 2020 la poblacion %s en %s era de %s. A continuación se muestra desglosada por edad" % (str_filtro_dic[json_flag['filtro']], nom_message, pob_filtro_message)
+            message_str = "En 2020 la poblacion %s en %s era de %s. A continuación se muestra desglosada por edad." % (str_filtro_dic[json_flag['filtro']], nom_message, pob_filtro_message)
             messages = to_json_simple_desglose(message_str, label_edad, data_pob)
             return messages
-        if json_flag['desglose'] == 'sexo':
-            if json_flag['filtro'] == 'infantil':
-                campos_rango_edad, campos_filtro_sexo = ['POB91, POB49'], 'POB91, POB49'
-            elif json_flag['filtro'] == 'joven':
+        if json_flag['desglose'] == 'Sexo':
+            if json_flag['filtro'] == 'Infantil':
+                campos_rango_edad, campos_filtro_sexo = ['POB91', 'POB49'], 'POB91, POB49'
+            elif json_flag['filtro'] == 'Juvenil':
                 campos_rango_edad, campos_filtro_sexo = ['POB94', 'POB52'], 'POB94, POB52'
-            elif json_flag['filtro'] == 'adulto':
+            elif json_flag['filtro'] == 'Adulta':
                 campos_rango_edad, campos_filtro_sexo = ['(POB97 + POB98) AS adulto', '(POB56 + POB57) AS adulto'], '(POB97 + POB98) AS adulto, (POB56 + POB57) AS adulto'
-            elif json_flag['filtro'] == 'mayor':
+            elif json_flag['filtro'] == 'Tercera edad':
                 campos_rango_edad, campos_filtro_sexo = ['POB106', 'POB65'], 'POB106, POB65'
             data_pob_sex = pob_edad(clave, tabla, campos_filtro_sexo, spark) 
             nom_message, pob_hombres_message = get_pob_message(clave, tabla, nombregeo, spark, campos_rango_edad[0])
             nom_message, pob_mujeres_message = get_pob_message(clave, tabla, nombregeo, spark, campos_rango_edad[1])
-            message_str = "En 2020 la poblacion %s de hombres en %s era de %s y la de mujeres era de %s." % (str_filtro_dic[json_flag['filtro']], nom_message, pob_hombres_message, pob_mujeres_message)
+            message_str = "En 2020 la poblacion %s masculina en %s era de %s y la femenina era de %s." % (str_filtro_dic[json_flag['filtro']], nom_message, pob_hombres_message, pob_mujeres_message)
             messages = to_json_simple_desglose(message_str, label_sexo, data_pob_sex)
             return messages
     elif filtro_flag == True: # aplicando filtro sin desglose 
         campos_filtro = campos_filtro_dic[json_flag['filtro']] # campos necesarios para cada filtro
         nom_message, pob_filtro_message = get_pob_message(clave, tabla, nombregeo, spark, campos_filtro)
-        message_str = "En 2020 la poblacion %s en %s era de %s" % (str_filtro_dic[json_flag['filtro']], nom_message,pob_filtro_message)
+        message_str = "En 2020 la poblacion %s en %s era de %s." % (str_filtro_dic[json_flag['filtro']], nom_message,pob_filtro_message)
         messages = to_json_simple_message(message_str)
         return messages
     
@@ -197,7 +203,7 @@ def flag_alert(json_flag, spark):
 
 def to_json_doble_desglose(nom_message, pob_message, label_edad, data_pob_hom, data_pob_muj):
     message_json = {"type": "graph_2",
-            "message":"En 2020 la poblacion de %s era de %s personas. A continuacion se muestra la poblacion desglosada por edad y sexo" % (nom_message, pob_message),
+            "message":"En 2020 la poblacion de %s era de %s personas. A continuacion se muestra la poblacion desglosada por edad y sexo." % (nom_message, pob_message),
             "graph": {
                 "labels": label_edad,
                 "datasets": [{
